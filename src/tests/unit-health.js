e526ae6998e8d6c80cebcef5b1aa538cdc820b23
Asweh Debrej <18221145@std.stei.itb.ac.id>
2023-11-28 23:31:42 +0700

    Update unit-health.js

--- src/tests/unit-health.js
+++ src/tests/unit-health.js
@@ -6,7 +6,7 @@ module.exports.options = {
 };
 
 module.exports.default = function () {
-    let res = http.get(`http://localhost:${__ENV.APP_PORT}/health`);
+    let res = http.get(`http://localhost:5001/health`);
     check(res, {
         'API server health is OK': (r) => r.json()["API Server"] === "OK"
     })
